6beff65856708448dd7c8530d41d00b9d6faf7b1
Jonas Vendelbo Hansen <Joni1234@hotmail.dk>
2020-03-09 12:18:55 +0100

    Added branch name generation

--- close_feature.sh
+++ close_feature.sh
@@ -7,3 +7,22 @@ git branch -d $1
 git tag -a $version -m "Develop Version: $version"
 git push -u origin develop
 
+
+
+# Create new version number and branch
+sh version_iteration.sh 1
+
+version=`cat version.txt`
+branchname=$1
+
+if [$1 == ""]
+then 
+	echo hej
+	$branchname=$version
+fi 
+
+git checkout -b $branchname develop
+git add version.txt
+git commit -m "Bumped version to $version"
+
+
--- open_feature.sh
+++ open_feature.sh
@@ -4,6 +4,11 @@ sh version_iteration.sh 1
 version=`cat version.txt`
 branchname=$1
 
+if [$1 == ""]
+then 
+	branchname=$version
+fi 
+
 git checkout -b $branchname develop
 git add version.txt
 git commit -m "Bumped version to $version"
