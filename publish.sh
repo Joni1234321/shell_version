5da11876aa5554059440d6da2d259657d7a87f23
Jonas Vendelbo Hansen <Joni1234@hotmail.dk>
2020-03-09 15:01:21 +0100

    Changed gitignore to *.txt

--- publish.sh
+++ publish.sh
@@ -1,5 +1,6 @@
 # Uploads developer branch to the master branch
 
+
 # Merge develop with master
 git checkout master || exit 1
 git merge --no-ff develop
@@ -7,6 +8,8 @@ git merge --no-ff develop
 
 # Commit and tag version
 sh version_iteration.sh 0 
+version=`cat version.txt`
+
 git add version.txt
 git commit -m "Bumped version to $version"
 git tag -a $version -m "$merge version: $version"
