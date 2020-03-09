5e26b7b9c9c097301d5a046069b411eb8a3d0993
Jonas Vendelbo Hansen <Joni1234@hotmail.dk>
2020-03-09 13:23:24 +0100

    Added a close.sh file that recognices branchtype and closes it properly

--- close.sh
+++ close.sh
@@ -1,9 +1,16 @@
 branchtype=$1
 
-if [[ -z "$branchtype" ]]; 
-then 
+if [[ -z "$branchtype" ]]; then 
 	branchtype=`git rev-parse --abbrev-ref HEAD`
 	branchtype=${branchtype##refs/heads/}
+	IFS='-' read -ra my_array <<< "$branchtype"
+	branchtype=${my_array[0]}
 fi
 
-echo $branchtype
+if [[$branchtype == "feature"]]; then 
+	sh "close_feature.sh" $1
+elif [[$branchtype == "patch"]]; then 
+	sh "close_patch.sh" $1
+elif [[$branchtype == "hotfix"]]; then 
+	sh "close_hotfix.sh" $1
+fi
