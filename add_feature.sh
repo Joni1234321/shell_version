b532f38cc6c3b48ffbea203b884cc20def3b47b5
Jonas Vendelbo Hansen <Joni1234@hotmail.dk>
2020-03-09 11:58:29 +0100

    test of feature scipt 1

--- add_feature.sh
+++ add_feature.sh
@@ -1,5 +1,7 @@
 # Close branch and merge with develop 
-git checkout develop
-git merge --no-ff $1
-git branch -d $1
-git push -u origin develop
+
+version=`cat version.txt`
+echo "$version"
+git checkout -b "$version"
+git checkout "$version"
+
--- create_feature.sh
+++ create_feature.sh
@@ -1,3 +1,8 @@
 # Create new version number and branch
 sh version_iteration.sh 1
 git checkout -b $1 develop
+
+git checkout develop
+git merge --no-ff $1
+git branch -d $1
+git push -u origin develop
